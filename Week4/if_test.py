7302ae60b3ecda823131563addb17d79d11fbc35
Steve McGuire <s.mcguire@hud.ac.uk>
2020-10-21 15:05:35 +0100

    Add hello

--- Week4/if_test.py
+++ Week4/if_test.py
@@ -3,4 +3,4 @@ i = int(input("Please enter a number greater than zero"))
 if i >= 0:
     print(i, "is greater than zero")
 else:
-    print(i, "I's less than zero!")
+    print(i, "is less than zero!")
